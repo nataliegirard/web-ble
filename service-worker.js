af3d8ca1a52960cce2018ffcc061a8e51e308486
Natalie Girard <girardnatalie@gmail.com>
2017-03-29 17:55:07 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["index.html","6db75229ca4beee659326a6ad211ce70"],["manifest.json","c6259927d1aae52b4a29a4b7c3dfb4fb"],["static/css/main.db226a3d.css","1eae9c7d54d4261e77a0c121df8f7845"],["static/js/main.25604230.js","056538b2c404a6e954061e6906d70613"],["static/media/candle-transparent.7af750df.png","7af750df95416cf9dd692efc8de087d4"]];
+var precacheConfig = [["index.html","d073c772bf090bc79d27413c7ce8ca3f"],["manifest.json","c6259927d1aae52b4a29a4b7c3dfb4fb"],["static/css/main.79bac379.css","605a776f8ae9fa01a8b1723b6d53e34b"],["static/js/main.d5f399f0.js","6650e9386c50039fc125b1bd6cd9cfc2"],["static/media/candle-transparent.7af750df.png","7af750df95416cf9dd692efc8de087d4"]];
 var cacheName = 'sw-precache-v2-sw-precache-' + (self.registration ? self.registration.scope : '');
 
 
